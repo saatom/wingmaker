19fc5b519cc026ff0dbbaa5949863fdc4cb0c6b4
Wyatt Richards <wr1701@proton.me>
2025-08-30 12:36:46 -0800

    Lots of bug fixes; GUI actually works now and produces (reasonably) reliable results

--- performance.m
+++ performance.m
@@ -38,6 +38,7 @@ function data=performance(wing, varargin)
   missing=[];
   c0=wing.sections{1}.chord; %initial chord length
   Re=rho*v_inf*c0/mu;
+  q_inf=.5*rho*v_inf^2; %freestream dynamic pressure
   %v=Re/(rho*c0)*mu; %find air velocity
   for i=1:sections
     sect=wing.sections{i};
@@ -50,37 +51,43 @@ function data=performance(wing, varargin)
     %fputs(insfid, ["load " secname "\n" secname(end-6:end) "\noper\npacc\n" datname "\n\naseq " num2str(amin) " " num2str(amax) " " num2str(ainc) "\npacc\n\nquit"]);
     fputs(insfid, [sect.identifier "\n" secname(end-6:end) "\noper\nvisc" num2str(Re*sect.chord/c0) "\niter 100\npacc\n" datname "\n\naseq " num2str(amin) " " num2str(amax) " " num2str(ainc) "\npacc\n\nquit"]);
     system(["xfoil < " insname], true);
-    dat=dlmread(datname, '', 12, 0)(:,1:5); %Load xfoil's data into octave; columns 1-5 are [ ALPHA , CL , CD , CDp , CM ]
+    rawdat=dlmread(datname, '', 12, 0)(:,1:5); %Load xfoil's data into octave; columns 1-5 are [ ALPHA , CL , CD , CDp , CM ]
+    alphas=avg(:,1); %list of alphas
+    dat=[alphas];
+    for j=2:5
+      dat=[dat interp1(rawdat(:,1), rawdat(:,j), alphas, "extrap")];
+    endfor
     data.raw{i}=dat;
     delete(secname, insname, datname); %Get rid of all the temporary files that were just created
     if i>1 %calculate weighted average for performance data
-      sec=wing.sections{i}; secp=wing.sections{i-1};
-      dp=sec.position-secp.position;
-      c1=secp.chord;
-      c2=sec.chord;
-      datp=data.raw{i-1};
-      valid=length(dat(:,1))==length(avg(:,1)); %check if there are as many data points in the latest XFOIL output as we expect
+      sec=wing.sections{i}; secp=wing.sections{i-1}; %sec = current section, secp = previous section
+      dp=sec.position-secp.position; %difference in position along wing between sections
+      c1=secp.chord; %chord of previous section
+      c2=sec.chord; %chord of current section
+      datp=data.raw{i-1}; %get data of previous section
+      valid=length(rawdat(:,1))==length(avg(:,1)); %check if there are as many data points in the latest XFOIL output as we expect
+      if valid != 1
+	msg=sprintf("%.f data points missing due to XFOIL divergence; using linear extrapolation to fill the gaps\n", length(avg(:,1))-length(rawdat(:,1)));
+	warning(msg);
+      endif
+      %{
       if valid != 1
 	localmissing=[]; %store the indices of the missing data points in this specific airfoil section
-	for j=1:length(avg(:,1))
+	for j=1:length(avg(:,1)) %go through data points and look for missing ones
 	  k=j-length(localmissing);
-	  if avg(j,1)~=dat(k,1)
-	    localmissing=[localmissing j]; %add this problematic data point to the list of missing ones
+	  if length(dat)<k || avg(j,1)~=dat(k,1) %check if each alpha in avg has a corresponding row in dat
+	    localmissing=[localmissing j] %add this problematic data point to the list of missing ones
 	  endif
 	endfor
 	dat=sort([dat; avg(localmissing,:)]);
 	missing=[missing localmissing];
       endif
-      ngood=length(datp);
-      localavg(ngood,1:4)=(c1.*datp(ngood,1:4)+c2.*dat(ngood,1:4))./(2)*dp; %find the average of the performance data in this particular section
-      localavg(ngood,5)=(c1^2.*datp(ngood,5)+c2^2.*dat(ngood,5))./(2)*dp; %find the average of the performance data in this particular section
-      avg=avg+[0 1 1 1 1].*localavg; %add the average performance stuff between two sections
+	%}
+      localavg(:,1:4)=(c1.*datp(:,1:4)+c2.*dat(:,1:4))./(2)*dp; %find the average of the performance data in this particular section
+      localavg(:,5)=(c1^2.*datp(:,5)+c2^2.*dat(:,5))./(2)*dp; %find the average of the performance data in this particular section
+      avg=avg+[0 1 1 1 1].*localavg*q_inf*.5; %add the average performance stuff between two sections
     endif
   endfor
-  missing=unique(sort(missing)); %remove contaminated data points from divergence
-  avg(missing,:)=inf; %set contaminated data points to infinity so that they're easy to remove
-  avg=avg(find(~isinf(avg(:,1))),:); %remove aforementioned data points
-  avg(:,2:end)=avg(:,2:end)*.5*rho*v_inf^2;
   data.polar=avg;
   if length(missing)>0
     warning([num2str(length(missing)) " data point(s) missing due to XFOIL divergence\n"]);
